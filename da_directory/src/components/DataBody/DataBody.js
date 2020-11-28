223ee41567c5292da57a96ac632efffafadbe083
Rocio Rios <rrocio46@yahoo.es>
2020-11-28 17:43:46 -0500

    working on components files

--- da_directory/src/components/DataBody/DataBody.js
+++ da_directory/src/components/DataBody/DataBody.js
@@ -1,18 +1,50 @@
-import react from 'react';
+import React from 'react';
 
 import './DataBody.css';
 
 const DataBody = ({ users }) => {
     const formatDate = (date) =>{
-
+     let dateArray = date.split("-");
+     let year = dateArray[0];
+     let month = dateArray[1];
+     let dayArray = dateArray[2].split("T");
+     let day = dayArray[0];
+     let formattedDate = [month, day, year].join("-");
+    return formattedDate;
     }
   return (
       <body>
-          {users[0] !== undefined && users[0].name !== undefined ? (
-              users.map(({ login, name, picture, phone, email, dob }) => {
-                  
-              })
-          )}
+       {users[0] !== undefined && users[0].name !== undefined ? (
+        users.map(({ login, name, picture, phone, email, dob }) => {
+          return (
+            <tr key={login.uuid}>
+              <td data-th="Image" className="align-middle">
+                <img
+                  src={picture.medium}
+                  alt={"profile image for " + name.first + " " + name.last}
+                  className="img-responsive"
+                />
+              </td>
+              <td data-th="Name" className="name-cell align-middle">
+                {name.first} {name.last}
+              </td>
+              <td data-th="Phone" className="align-middle">
+                {phone}
+              </td>
+              <td data-th="Email" className="align-middle">
+                <a href={"mailto:" + email} target="__blank">
+                  {email}
+                </a>
+              </td>
+              <td data-th="DOB" className="align-middle">
+                {formatDate(dob.date)}
+              </td>
+            </tr>
+          );
+        })
+      ) : (
+        <></>
+      )}
       </body>
   )
 }
--- da_directory/src/utils/API.js
+++ da_directory/src/utils/API.js
@@ -1,8 +1,8 @@
-import axios from "axios";
+// import axios from "axios";
 
-export default {
-  // Gets all users
-  getUsers: () => {
-    return axios.get("https://randomuser.me/api/?results=200&nat=us");
-  }
-};
+// export default {
+//   // Gets all users
+//   getUsers: () => {
+//     return axios.get("https://randomuser.me/api/?results=200&nat=us");
+//   }
+// };
